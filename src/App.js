5f2d8089140f24d8796a36fb5c14601b42e01dc2
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-08 12:45:54 +0600

    payload

--- src/App.js
+++ src/App.js
@@ -10,12 +10,20 @@ function App() {
   const decrement = () => {
     dispatch({ type: 'DECREMENT' }); 
   };
+  const add_ten = () => {
+    dispatch({ type: 'ADD_TEN', payload: 10 });
+  };
+  const min_ten = () => {
+    dispatch({ type: 'MIN_TEN', payload: 10 });
+  };
   return (
     <div>
       <h1>Counter App </h1>
       <h2>{counter } </h2>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
+      <button onClick={add_ten}>Add 10</button>
+      <button onClick={min_ten}>Minus 10</button>
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,16 @@ import { createStore } from "redux";
 const reducerFn = (state = {counter: 10}, action) => {
 if (action.type === "INCREMENT") {
   return { counter: state.counter + 1 };
-} else if (action.type === "DECREMENT") {
+}
+ if (action.type === "DECREMENT") {
     return { counter: state.counter - 1 };
     }
+    if (action.type === "ADD_TEN") {
+        return { counter: state.counter + action.payload };
+        }
+        if (action.type === "MIN_TEN") {
+            return { counter: state.counter - action.payload };
+            }
 return state;
 };
 
